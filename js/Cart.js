91179a7002dc58935f9b697e47a1b6d61735450b
dnanusevski <dnanusevski@gmail.com>
2023-01-14 14:36:01 +0100

    some stage like ...

--- js/Cart.js
+++ js/Cart.js
@@ -1,8 +1,22 @@
 export default class Cart {
-	// a method for building HTML component
-	addToCart(id, amount) {
-        console.log(id+amount);
-    }
 
-	
+    items = [];
+
+	// Add if exists, else add amount
+	addToCart(id, amount) {
+       // in app wide state, we should clone or map the array.
+        let item = {id:id, amount:parseInt(amount)};
+        let el = this.items.find(obj => obj.id === id)
+        
+        if(el){
+            el.amount += parseInt(amount);
+        } else {
+            this.items.push(item);
+        }
+        console.log(this.items);
+    }
+    
+    removeFromCart(id){
+        console.log(id);
+    }
 }
--- js/app.js
+++ js/app.js
@@ -6,24 +6,21 @@ import Cart from "./Cart.js";
 let APIClient = new ApiClient();
 let productRepo = new PorductRepository(APIClient);
 let scf = new SearchComponentFactory();
-console.log(appCart);
 appCart = new Cart();
 
-//console.log(cart.addToCart);
 
 let products = await productRepo.getProduct('SearchProducts');
-let product = await productRepo.getProduct('GetProduct', {id: 1});
+//let product = await productRepo.getProduct('GetProduct', {id: 1});
 let html = await scf.getComponentHtml(products);
 
-/*
-console.log(products);
-console.log(product);
-console.log(html);
-*/
 
 document.getElementById('search_products_container').innerHTML = html;
 
-
+goToCart = () => {
+    alert('hello');
+    document.getElementById('search_products_container').innerHTML = "";
+    document.getElementById('search_products_container').style.display = "none";
+}
 
 
 /*
